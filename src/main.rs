b36bb384917f35de89232b1e2689e7b13ebe57e4
JoshM-Yoru <jmaciel335i@gmail.com>
2022-10-24 17:28:04 -0400

    First commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use yew::prelude::*;
+
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    
+}
+fn main() {
+    println!("Hello, world!");
+}
